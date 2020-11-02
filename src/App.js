788a10749d0e0ab0f1e2817d4b61a6b27bd69cfb
Saul Flores <saulpolo95@gmail.com>
2020-11-01 17:38:55 -0800

    initial commit added the main tiles to the homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  HomePage from './home-page/components/homepage'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
