6b019cfb0175095b93eeeb401a2a476bfb950171
Saul Flores <saulpolo95@gmail.com>
2020-11-07 09:26:37 -0800

    added cart actions, reducer, types that allow for the dropdown be hidden, added cart dropdown, added signin redirect. Dropdown still does not add items to it, added shpping bag svg

--- src/components/header/components/header.js
+++ src/components/header/components/header.js
@@ -4,7 +4,10 @@ import '../styles/header.scss'
 import { auth } from '../../../firebase/firebase.utils'
 import { connect } from 'react-redux' 
 import { ReactComponent as Logo } from '../../../assets/crown.svg'
-const Header = ({ currentUser }) => (
+import CartIcon from '../../cart-icon/cart-icon.component'
+import CartDropdown from '../../cart-dropdown/cart-dropdown.component'
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to="/">
             <Logo className='logo' />
@@ -19,12 +22,16 @@ const Header = ({ currentUser }) => (
             {
                 currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :  <Link className='option' to='signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
+        
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
