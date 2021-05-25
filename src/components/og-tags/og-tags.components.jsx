c12075dbb6e5bc3b50fb2bec8e3644d34409b964
Saul Flores <saulpolo95@gmail.com>
2021-05-25 16:16:26 -0700

    added improvment to og tags

--- src/components/og-tags/og-tags.components.jsx
+++ src/components/og-tags/og-tags.components.jsx
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import AppendHead from 'react-append-head';
 
 const WithOgTags = WrappedComponent => {
-    const OgTags = ({ title, description, imgUrl,...otherProps }) => (
+    const OgTags = ({ title, description, imgUrl, ...otherProps }) => {
+        console.log("OgTags: title", title)
+        console.log("OgTags: description", description)
+        console.log("OgTags: imgUrl", imgUrl)
+        return (
         <div>
             <AppendHead>
                 <title>{title}</title>
-                <meta property='og:type' content='article' />
-                <meta property='og:title' content={title} />
-                <meta property='og:description' content={description} />
-                <meta property='og:image' content={imgUrl} />
-                <meta property='og:image:width' content='640' />
-                <meta property='og:image:height' content='442' />
+                <meta name='og-type' property='og:type' content='article' />
+                <meta name='og-title' property='og:title' content={title} />
+                <meta name='og-description' property='og:description' content={description} />
+                <meta name='og-image' property='og:image' content={imgUrl} />
+                <meta name='og-image-width' property='og:image:width' content='640' />
+                <meta name='og-image-height' property='og:image:height' content='442' />
             </AppendHead>
             <WrappedComponent {...otherProps}/>
         </div>
-    )
+    )}
     return OgTags
 }
 
